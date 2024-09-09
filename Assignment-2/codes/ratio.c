e4cda4d8f56a8ce5deb77de3d4c886e17480102f
Balaji29-code <balajibalamurugan29@gmail.com>
2024-09-09 23:39:34 +0530

    EE1030

--- Assignment-2/codes/ratio.c
+++ Assignment-2/codes/ratio.c
@@ -1,30 +1,68 @@
+#include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+
+#include "libs/matfun.h"
+#include "libs/geofun.h"
+
+float find_k( double , double ,double  );
 
 int main()
 {
-    FILE *file = fopen("point.txt", "w"); 
-    if (file == NULL)
-    {
-        perror("Error opening file");
-        return 1; 
+
+    float x1 = 2 , x2 = 3 , y1 = -2 , y2 = 7 , x3 = (float)24/11 ;
+    float y3;
+    int m = 2 , n = 1;
+    double **P, **Q, **R , **temp;
+
+    P = createMat(m,n);
+    Q = createMat(m,n);
+    R = createMat(m,n);
+    temp = createMat(m,n);
+
+    P[0][0] = x1;
+    P[1][0] = y1;
+    Q[0][0] = x2;
+    Q[1][0] = y2;
+    R[0][0] = x3;
+    float k = find_k(P[0][0], Q[0][0], R[0][0] );
+
+     FILE *file = fopen("point.txt", "w");
+    if (file == NULL) {
+        fprintf(stderr, "Error opening file.\n");
+        return 1;
     }
-
-    double k , yR; //Variables to be found
-    double P[2][1] = {2 , -2}; //Co-ordinates of the point P
-    double Q[2][1] = {3 , 7}; //Co-Ordinate of the point Q
-    double R[2][1] ; 
-    R[0][0] = (float)24/11; // X-coordinate of R 
-    // Finding K using the x-coordinates of the P, Q and R
+        
    
-     k = (R[0][0] - P[0][0]) / (Q[0][0] - R[0][0]);
+   temp = Matscale( Q,  m,  n,  k);
+   temp = Matadd(temp, P , m , n);
+   temp = Matscale(temp ,m, n, 1.0/(k+1) );
+   R = temp;
 
-     //Find the value of y using the K we found from the above equation
-     yR = (k * Q[1][0] + P[1][0]) / (k + 1);
-     R[1][0] = yR; // Y-coordinate of R
-     fprintf(file, "%.2f  %.2f \n", P[0][0], P[1][0]);
-     fprintf(file, "%.2f  %.2f \n", Q[0][0], Q[1][0]);
-     fprintf(file, "%.2f  %.2f \n", R[0][0], R[1][0]);
+   fprintf(file, "%f %f\n", P[0][0], P[1][0]);
+    fprintf(file, "%f %f\n", Q[0][0], Q[1][0]);
+    fprintf(file, "%f %f\n", R[0][0], R[1][0]);
+     fclose(file);
 
-     fclose(file); //Ends the file
-     
+   
+
+
+
+
+    
+    
 }
+
+float find_k( double a, double b,double c )
+{
+    float k ;
+    k = (float)(a - c) / (c - b);
+    return k ;
+
+
+}
+
