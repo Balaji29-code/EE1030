08605774ec4990a56e1cd52d3503f720cf8bf678
Balaji29-code <balajibalamurugan29@gmail.com>
2024-09-15 13:29:53 +0530

    EE1030

--- Assignment-3/codes/distance.c
+++ Assignment-3/codes/distance.c
@@ -5,18 +5,24 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <unistd.h>
-
 #include "libs/matfun.h"
 #include "libs/geofun.h"
 
+void point_gen(FILE *fptr, double **A, double **B, int num_points) {
+    for (double i = 0; i <= num_points; i++) {
+        double **output = Matsec(A, B, 2, (double) i / num_points);
+        fprintf(fptr, "%lf %lf\n", output[0][0], output[1][0]);
+        freeMat(output,2);
+    }
+}
+
 int main()
 {
-    float a, b;
-    float x1 = 0, y1 = 0;
-    a = 2;
-    b = 3;
-    float x2 = (a - b);
-    float y2 = (a + b);
+    float a = 2 , b = 3;
+    float x1, y1, x2, y2;
+    x1 = 0 , y1 = 0;
+    x2 = a - b ;
+    y2 = a + b;
 
     int m = 2, n = 1;
     double **A = createMat(m, n);
@@ -27,17 +33,26 @@ int main()
     B[0][0] = x2;
     B[1][0] = y2;
 
-    FILE *file = fopen("distance.txt", "w");
-    if (file == NULL) {
-        fprintf(stderr, "Error opening file.\n");
+    FILE *fptr;
+    fptr = fopen("points.txt", "w");
+    if (fptr == NULL) {
+        printf("Error opening file!\n");
         return 1;
     }
-    
-    double **s_ab = Matsub(A, B, m, n);
-    double sideAB = Matnorm(s_ab, m);
-    fprintf(file, "%f %f\n", A[0][0], A[1][0]);
-    fprintf(file, "%f %f\n", B[0][0], B[1][0]);
-    fprintf(file, "The distance between the points is %lf" ,sideAB);
+
+     point_gen(fptr, A, B, 40);
+     fprintf(fptr,"%lf %lf\n",B[0][0],B[1][0]);
+
+     double **s_ab = Matsub(A, B, m, n);
+     double sideAB = Matnorm(s_ab, m);
+
+    freeMat(A,m);
+    freeMat(B,m);
+    freeMat(s_ab,m);
+
+    fprintf(fptr, "The distance between the points for a = 2 and b = 3 is  %lf\n", sideAB);
+    fclose(fptr);
+    return 0;
 
 
 }
--- Assignment-3/codes/plot.py
+++ Assignment-3/codes/plot.py
@@ -1,28 +1,22 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 
 # Load the points from the text file
-points = np.loadtxt("distance.txt", delimiter=' ',max_rows=2)
+points = np.loadtxt("points.txt", delimiter=' ', max_rows=len(list(open("./points.txt")))-1)
 
 # Extract the x and y coordinates
 x = points[:, 0]
 y = points[:, 1]
 
-A = np.array([0, 0])
-B = np.array([-1, 5])
+plt.figure()
+plt.plot(x, y, label='lines')
 
-
-
-plt.figure()  # Create a new figure for the plot
-plt.plot(A[0], A[1], 'ro', label='Point A')  # Red dot for point A
-plt.plot(B[0], B[1], 'bo', label='Point B')  # Blue dot for point B
-
-
-
-plt.plot(x, y, label='Line')
+plt.scatter(x[0], y[0], c = 'c', label = 'point P')
+plt.scatter(x[41], y[41], c = 'm', label = 'point Q')
 plt.xlabel("x")
 plt.ylabel("y")
-plt.title("Line passing given points")
+plt.title("The lines formed by the points  ")
 plt.grid(True)
 plt.legend()
 
